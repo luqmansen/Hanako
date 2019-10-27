9c2a4a5cb33e16ea00b339daca182ba2c6d040fc
luqmansen <luqmansen@gmail.com>
2019-10-27 11:02:36 +0700

    alter http response code

--- controllers/animeControllers.go
+++ controllers/animeControllers.go
@@ -14,9 +14,9 @@ var GetAll = func(w http.ResponseWriter, r *http.Request) {
 
 	data := models.GetAll(show)
 	if data == nil {
-		u.Respond(w, u.Message(false, "Not Found"))
+		u.Respond(w, u.Message(http.StatusNoContent, "Not Found"))
 	} else {
-		resp := u.Message(true, "Success")
+		resp := u.Message(http.StatusOK, "Success")
 		resp["data"] = data
 		u.Respond(w, resp)
 	}
@@ -27,9 +27,9 @@ var GetByTitle = func(w http.ResponseWriter, r *http.Request) {
 	keyword := r.URL.Query().Get("title")
 	data := models.GetByTitle(keyword)
 	if data == nil {
-		u.Respond(w, u.Message(false, "Not Found"))
+		u.Respond(w, u.Message(http.StatusNoContent, "Not Found"))
 	} else {
-		resp := u.Message(true, "Success")
+		resp := u.Message(http.StatusOK, "Success")
 		resp["data"] = data
 		u.Respond(w, resp)
 	}
@@ -40,12 +40,12 @@ var GetById = func(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id, err := strconv.Atoi(params["id"])
 	if err != nil {
-		u.Respond(w, u.Message(false, "There was an error in your request"))
+		u.Respond(w, u.Message(http.StatusBadRequest, "There was an error in your request"))
 	} else if data := models.GetByID(uint(id)); data == nil {
-		u.Respond(w, u.Message(false, "Not Found"))
+		u.Respond(w, u.Message(http.StatusNoContent, "Not Found"))
 	} else {
 
-		resp := u.Message(true, "Success")
+		resp := u.Message(http.StatusOK, "Success")
 		resp["data"] = data
 		u.Respond(w, resp)
 	}
--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -12,7 +12,7 @@ var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account)
 	if err != nil {
-		u.Respond(w, u.Message(false, "Invalid Request"))
+		u.Respond(w, u.Message(http.StatusBadRequest, "Invalid Request"))
 		return
 	}
 	resp := account.Create()
@@ -24,7 +24,7 @@ var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account)
 	if err != nil {
-		u.Respond(w, u.Message(false, "Invalid Request"))
+		u.Respond(w, u.Message(http.StatusBadRequest, "Invalid Request"))
 		return
 	}
 	resp := models.Login(account.Email, account.Password)
