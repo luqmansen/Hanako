cb033e2dca43cdba3953379a7ba9a775af9ee221
luqmansen <luqmansen@gmail.com>
2020-03-26 17:29:55 +0700

    WIP add jaeger tracer

---
+++ tracer/tracer.go
@@ -0,0 +1,33 @@
+package tracer
+
+import (
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"io"
+	"time"
+)
+
+func NewTracer(servicename string, addr string) (opentracing.Tracer, io.Closer, error) {
+	cfg := jaegercfg.Configuration{
+		ServiceName: servicename,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			BufferFlushInterval: 1 * time.Second,
+			LogSpans:            true,
+		},
+	}
+
+	sender, err := jaeger.NewUDPTransport(addr, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	reporter := jaeger.NewRemoteReporter(sender)
+	tracer, closer, err := cfg.NewTracer(jaegercfg.Reporter(reporter))
+
+	return tracer, closer, err
+}
